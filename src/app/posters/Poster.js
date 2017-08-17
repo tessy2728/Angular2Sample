d2ee82671d5ad794936b3ad1cc408240435c55af
Tessygit <tessy@qburst.com>
2017-08-17 23:58:27 +0530

    list style

--- src/app/posters/Poster.js
+++ src/app/posters/Poster.js
@@ -1,8 +1,6 @@
 "use strict";
 var Poster = (function () {
-    function Poster(
-        //public id: string,
-        name, poster_image) {
+    function Poster(name, poster_image) {
         this.name = name;
         this.poster_image = poster_image;
     }
--- src/app/posters/Poster.ts
+++ src/app/posters/Poster.ts
@@ -1,6 +1,5 @@
 export class Poster {
     constructor(
-        //public id: string,
         public name: string,
         public poster_image: string
     ) { }
